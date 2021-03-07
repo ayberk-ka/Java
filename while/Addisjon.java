8b1b398edae2dca1f572b941b4cc4e685b9b40ff
Mahmoud <ayberk.mh@gmail.com>
2021-03-07 08:52:53 +0100

    Et program som plusser alle tallene sammen mellom 10 og 100

---
+++ while/Addisjon.java
@@ -0,0 +1,15 @@
+public class Addisjon
+{
+    public static void main(String[] args)
+    {
+        int x = 10;
+        int y = 0;
+        
+        while ( x<=100)
+        {
+            y = y+x;
+            x = x+1;
+        }
+        System.out.println("Sum: "+y);
+    }
+}
